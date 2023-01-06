f3958ea4a5060637daabff12c3dba303a90a26a0
mamoor <mamoor99@gmail.com>
2023-01-06 20:14:48 +0530

    F AC POC4

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import '../styles/Form.css'
 import { useNavigate } from 'react-router-dom'
 
 const Form = () => {
   const [signInForm, setSignInForm] = useState({ userName: '', password: '' })
+  const username = useRef(null)
+  const pwd = useRef(null)
 
   const navigate = useNavigate()
   const submitForm = (e) => {
@@ -15,16 +17,25 @@ const Form = () => {
   return (
     <div className='form--page'>
       <form method='POST' onSubmit={submitForm} autoComplete='off'>
+        <h4>POC-4</h4>
         <div className='form--group'>
           <label htmlFor=''>Username</label>
           <input
             type='text'
             name='userName'
+            readOnly
+            ref={username}
             autoComplete='off'
             value={signInForm.userName}
             onChange={(e) =>
               setSignInForm({ ...signInForm, [e.target.name]: e.target.value })
             }
+            onFocus={() => {
+              username.current.removeAttribute('readonly')
+            }}
+            onBlur={() => {
+              username.current.addAttribute('readonly')
+            }}
           />
         </div>
         <div className='form--group'>
@@ -32,11 +43,19 @@ const Form = () => {
           <input
             type='password'
             name='password'
-            autoComplete='off'
+            readOnly
+            ref={pwd}
+            autoComplete='new-password'
             value={signInForm.password}
             onChange={(e) =>
               setSignInForm({ ...signInForm, [e.target.name]: e.target.value })
             }
+            onFocus={() => {
+              pwd.current.removeAttribute('readonly')
+            }}
+            onBlur={() => {
+              pwd.current.addAttribute('readonly')
+            }}
           />
         </div>
         <div className='form--group'>
